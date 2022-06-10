e59351efe152618290dde1cbdd34206c024d5ba3
kk202393 <39188242+kk202393@users.noreply.github.com>
2022-06-10 15:39:01 +0530

    new change

--- Womensworls/settings.py
+++ Womensworls/settings.py
@@ -130,10 +130,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,STATIC_URL)
-STATICFILES_DIRS = [
-    BASE_DIR /"static"
-]
+STATIC_ROOT = os.path.join(BASE_DIR,STATIC_URL)
+# STATICFILES_DIRS = [
+#     BASE_DIR /"static"    <-- ye hai static file -->
+# ]
+
 # EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL ='/media/'
